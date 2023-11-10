42cbd708efe13c39453208d2165a6abb44f914ce
zaynlannister <zaynlannister@gmail.com>
2023-11-10 21:20:11 +0500

    refactor: code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import FeedSection from "./components/Feed/FeedSection";
-import FooterSection from "./components/Footer/FooterSection";
 import Header from "./components/Header/Header";
 import StepsSection from "./components/StepsSection/StepsSection";
 import TourSection from "./components/TourSection/TourSection";
@@ -19,7 +18,7 @@ const App = () => {
       <div className="py-14">
         <FeedSection />
       </div>
-      <div className="bg-[#F8F8F8] py-10">{/* <FooterSection /> */}</div>
+      {/* <div className="bg-[#F8F8F8] py-10"><FooterSection /></div> */}
     </div>
   );
 };
